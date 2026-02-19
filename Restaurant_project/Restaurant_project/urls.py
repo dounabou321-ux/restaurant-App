7a944d6b5faaed431caf455fc9b3dcc9afbcba3a
dounabou321-ux <dounabou321@gmail.com>
2026-02-19 23:42:34 +0100

    view

--- Restaurant_project/Restaurant_project/urls.py
+++ Restaurant_project/Restaurant_project/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from Baseapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',HomeView,name='home'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
